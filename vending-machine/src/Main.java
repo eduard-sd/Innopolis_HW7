847414eb16c51ceb39e422d86f8512d30c7e5adc
Eduard-sd <pr-spektr@yandex.ru>
2018-09-03 18:15:02 +0400

    Added if statment looking that happening. Still working with users interface

--- vending-machine/src/Main.java
+++ vending-machine/src/Main.java
@@ -7,9 +7,9 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-//     public static ColdDrinks[]  drinksNew = new ColdDrinks[4];
+    //     public static ColdDrinks[]  drinksNew = new ColdDrinks[4];
     public static void main(String[] args) {
-/*        VendingMachine vm = new VendingMachine();*/
+        /*        VendingMachine vm = new VendingMachine();*/
 /*        ColdDrinks fanta = new ColdDrinks("Fanta",5.5, Currency.Rub); // могу ли я вместо значений пердать input пользователя присвоенного к переменной
         ColdDrinks coca = new ColdDrinks("Coca-cola",4.7, Currency.Dinnar);
         ColdDrinks sprite = new ColdDrinks("Sprite",3.2, Currency.Usd);
@@ -35,53 +35,61 @@ public class Main {
 
         SecondVendingMachine svm = new SecondVendingMachine();
         svm.setType(DrinkType.HOT);
-        Drink tea = new Drink(50,"TEA",Currency.USD,DrinkType.HOT);
-        Drink coffee = new Drink(40,"Coffee",Currency.EURO,DrinkType.HOT);
-        Drink milk = new Drink(30,"Milk",Currency.USD,DrinkType.HOT);
-        Drink water = new Drink(40,"Water",Currency.RUB,DrinkType.HOT);
-        Drink greenTea = new Drink(1,"Green-Tea",Currency.EURO,DrinkType.HOT);
-        Drink chocolate = new Drink(4,"Chocolate",Currency.DINNAR,DrinkType.HOT);
+        Drink tea = new Drink(50, "TEA", Currency.USD, DrinkType.HOT);
+        Drink coffee = new Drink(40, "Coffee", Currency.EURO, DrinkType.HOT);
+        Drink milk = new Drink(30, "Milk", Currency.USD, DrinkType.HOT);
+        Drink water = new Drink(40, "Water", Currency.RUB, DrinkType.HOT);
+        Drink greenTea = new Drink(1, "Green-Tea", Currency.EURO, DrinkType.HOT);
+        Drink chocolate = new Drink(4, "Chocolate", Currency.DINNAR, DrinkType.HOT);
 
-        Drink[] drinkArr = new Drink[] {tea, coffee,milk,water,greenTea,chocolate}; //массив таких обьектов
+        Drink[] drinkArr = new Drink[]{tea, coffee, milk, water, greenTea, chocolate}; //массив таких обьектов
         svm.setDrinks(drinkArr); // положили в машину
         System.out.println(Arrays.toString(svm.getDrinks())); // [null, null, null, null, null]
 
-        Drink[] drinkArr2 = new Drink[] {tea, coffee,milk,water,greenTea}; //массив таких обьектов
+        Drink[] drinkArr2 = new Drink[]{tea, coffee, milk, water, greenTea}; //массив таких обьектов
         svm.setDrinks(drinkArr2); // положили в машину
-        System.out.println("Повторно: "+Arrays.toString(svm.getDrinks())); // Повторно: [TEA, Coffee, Milk, Water, Green-Tea]
+        System.out.println("Повторно: " + Arrays.toString(svm.getDrinks())); // Повторно: [TEA, Coffee, Milk, Water, Green-Tea]
+
+        Drink[] drinkArr3 = new Drink[]{tea, coffee, milk, water}; //массив таких обьектов
+        svm.setDrinks(drinkArr3); // положили в машину
+        System.out.println("Повторно массив 2: " + Arrays.toString(svm.getDrinks())); // Повторно: [TEA, Coffee, Milk, Water, Green-Tea]
 
         System.out.println(svm.getDrink(1));
         System.out.println(Arrays.toString(svm.getDrinks()));
 
 
         System.out.println("Вейдинговая машина 1.0");
-        System.out.println("Меню: "+"\nполучить список горячих напиток - 1"+"\nполучить список холодный напиток - 2"+"\nвыход - 3");
+        System.out.println("Меню: " + "\nполучить список горячих напиток - 1" + "\nполучить список холодный напиток - 2" + "\nвыход - 3");
 
         Scanner sc = new Scanner(System.in);
         int number = 0;
-        while(number != 3){
+        while (number != 3) {
             System.out.print("Введите число ");
             Scanner scanner = new Scanner(System.in);
-            while(!scanner.hasNextInt()){ //проверка не равно числу
+            while (!scanner.hasNextInt()) { //проверка не равно числу
                 System.out.println("Пожалуйста введите только целые числа, без букв и знаков препинания.");
                 scanner.next();
             }
             number = scanner.nextInt();
-            if(number == 1){
-                System.out.println("Горячие напитки: "+"\n"+Arrays.toString(svm.getDrinks())); // Повторно: [TEA, Coffee, Milk, Water, Green-Tea]
+            if (number == 1) {
+                System.out.println("Горячие напитки: " + "\n" + Arrays.toString(svm.getDrinks())); // Повторно: [TEA, Coffee, Milk, Water, Green-Tea]
                 System.out.println("Введите название напитка");
-                String userChoice = scanner.next();
-                if(ArrayUtils.contains(svm.getDrinks(), userChoice){// как сравнить ввод с списком напитков
-                    System.out.println("ваш напиток");
-                    System.out.println(svm.getDrink(1))
-                }
-            }else if(number == 2) {
+                Scanner scc = new Scanner(System.in);
+                String userChoice = scc.nextLine();
 
-            } else{
+                for (Drink s : svm.getDrinks()) {
+                    if (s.getName().equals(userChoice)) {
+                        // как узнать из какого массива выполилось условие if
+                        System.out.println("ваш напиток");
+                        System.out.println(s.getName());
+                    }
+                }/*else if(number == 2) {
+                continue;
+            }else{
                 System.out.println("Вы ввели не действующую кнопку");
                 continue;
+            }*/
             }
         }
     }
-
 }
